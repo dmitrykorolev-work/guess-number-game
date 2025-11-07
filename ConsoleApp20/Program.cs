86beb85aef20800d66f6048d48eecd283eb62798
dmitrykorolev-work <dmitrykorolev.work@gmail.com>
2025-11-07 03:59:49 +0200

    - Replaced fixed magic number with random value between 0 and 64 - Calculated max attempts dynamically using log2 of range - Added hint system ("higher"/"less") after each wrong guess - Improved feedback messages and color usage - Added special congratulatory message for first-try guesses

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -4,24 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        int magicNumber = 50;
+        int randomMin = 0;
+        int randomMax = 64;
+
+        int maxAttempts = (int) Math.Ceiling(Math.Log2(randomMax));
+
+        int magicNumber = new Random().Next(randomMin, randomMax);
+
+        int attempts = maxAttempts;
 
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Welcome to guess number game");
         Console.ResetColor();
 
-        int attempts = 3;
         while (attempts >= 1)
         {
             Console.Write("Enter x: ");
             int x = Convert.ToInt32(Console.ReadLine());
 
-            if (x == 50)
+            if (x == magicNumber)
             {
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine("Congrats, you guessed the number");
-                Console.ResetColor();
-                break;
+                if (attempts == maxAttempts)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Congratulations! You guessed the number on your first try!");
+                    Console.ResetColor();
+                    break;
+                } else{
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Congratulations! You guessed the number in {maxAttempts - attempts + 1} attempts!");
+                    Console.ResetColor();
+                    break;
+                }
             }
 
             attempts--;
@@ -34,8 +48,11 @@ class Program
                 break;
             }
             
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine($"Try again... (attempts: {attempts})");
+
+            string clue = x > magicNumber ? "less" : "higher";
+            Console.WriteLine($"The hidden number is {clue}");
             Console.ResetColor();
         }
     }
